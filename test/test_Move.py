893b777bfd25caa4c500865a3acd4b91ecc34a2d
3g0 <u.sohier@gmail.com>
2018-06-25 23:53:49 +0200

    Commit from Home (PC Windows)
    Moving files and creating folder with the files name (move them to the designated folder)

--- test/test_Move.py
+++ test/test_Move.py
@@ -1,9 +1,12 @@
 import os
 import shutil
 
-os.chdir('//3g0-fnas/Media/Video/Movie')
+PATH='//3g0-fnas/Media/Video/AnimeMovie/Walt Disney'
+PATH2=PATH+'/'
+
+os.chdir(PATH)
 for name in os.listdir("."):
     if name.endswith(".mkv"):
         print(name)
-        os.mkdir('//3g0-fnas/Media/Video/Movie/'+name.replace(".mkv",""))
-        shutil.move('//3g0-fnas/Media/Video/Movie/'+name, '//3g0-fnas/Media/Video/Movie/'+name.replace(".mkv", "")+ '/' + name)
+        os.mkdir(PATH2+name.replace(".mkv", ""))
+        shutil.move(PATH2+name, PATH2+name.replace(".mkv", "")+ '/' + name)
